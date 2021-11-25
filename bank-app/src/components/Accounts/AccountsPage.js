f73f01df0cfb551723e896eb6659311065d01dfa
Michael Siemens Uy <89561283+mspuy3@users.noreply.github.com>
2021-11-25 19:21:43 +0800

    Added AccountsPage,js

---
+++ bank-app/src/components/Accounts/AccountsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AccountsPage() {
+  return (
+   <div>
+      <h1>Accounts</h1>
+      <h2>List of All Accounts</h2>
+      <h2>Create New Account</h2>
+   </div>
+  )
+}
+
+export default AccountsPage;
