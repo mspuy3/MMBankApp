95040fc1e6dd3ed9e58e3fd577bb770e9718f67e
markcruz389 <markanthonycruz1230@gmail.com>
2021-11-25 20:26:55 +0800

    Adds Header Navigation

--- bank-app/src/components/Layout.js
+++ bank-app/src/components/Layout.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router";
+import Header from "./common/Header";
 
 function Layout() {
   return (
     /* A "layout route" is a good place to put markup you want to
        share across all the pages on your site, like navigation. */
-
-    // Navigation component here
-
-    <Outlet />
+    <div>
+      <Header />
+      <Outlet />
+    </div>
   );
 }
 
