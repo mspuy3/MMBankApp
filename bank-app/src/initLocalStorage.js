bc3dcd41b2bda086f0041766be4620804d0f1819
Michael Siemens Uy <89561283+mspuy3@users.noreply.github.com>
2021-11-30 21:40:28 +0800

    Improve Test Accounts feature
    Added 200 dummy accounts that were auto-generated.
    Accounts are inside 'testAccounts.json'.
    Edited initLocalStorage.js to import accounts from tesAccounts.json.

--- bank-app/src/initLocalStorage.js
+++ bank-app/src/initLocalStorage.js
@@ -3,44 +3,8 @@
 // Service that initialize accounts array into empty array in local storage
 // Service that initialize accounts array with dummy accounts in local storage
 
-var testAccounts = [
-  {
-    id: 1,
-    accountNumber: "00001",
-    accountName: "ALEX CROSS",
-    accountHolder: {
-      firstName: "Alex",
-      middleName: "Bourne",
-      lastName: "Cross",
-    },
-    balanceAmount: 1000.0,
-    transactionHistory: [],
-  },
-  {
-    id: 2,
-    accountNumber: "00002",
-    accountName: "JASON BOURNE",
-    accountHolder: {
-      firstName: "Jason",
-      middleName: "Cross",
-      lastName: "Bourne",
-    },
-    balanceAmount: 1000.0,
-    transactionHistory: [],
-  },
-  {
-    id: 3,
-    accountNumber: "00003",
-    accountName: "JACK BAUER",
-    accountHolder: {
-      firstName: "Jack",
-      middleName: "Twofour",
-      lastName: "Bauer",
-    },
-    balanceAmount: 1000.0,
-    transactionHistory: [],
-  },
-];
+import testAccounts from './testAccounts.json';
+
 
 export default function initLocalStorage() {
   initAccounts();
