6c5699ae5e9628caa31bf448c58d095ca4c42ef6
markcruz389 <markanthonycruz1230@gmail.com>
2021-12-07 20:51:57 +0800

    Fixes nav bug

--- bank-app/src/components/Layout.js
+++ bank-app/src/components/Layout.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router";
 import Header from "./common/Header";
+import { getLoggedInUser } from "../repositories/userRepository";
 
 function Layout() {
+  const [loggedInUser, setLoggedInUser] = useState(getLoggedInUser());
+
+  useEffect(() => {
+    setLoggedInUser(getLoggedInUser());
+  }, [loggedInUser]);
+
   return (
     /* A "layout route" is a good place to put markup you want to
        share across all the pages on your site, like navigation. */
 
     <div>
-      <Header />
+      {Object.keys(loggedInUser).length > 0 && (
+        <Header loggedInUser={loggedInUser} />
+      )}
       <Outlet />
     </div>
   );
