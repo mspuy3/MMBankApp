e4f5d026dde2ef6613b6817a5434196ca8376468
markcruz389 <markanthonycruz1230@gmail.com>
2021-12-11 06:51:55 +0800

    Applies style to manage account page

--- bank-app/src/components/Accounts/AccountForm.js
+++ bank-app/src/components/Accounts/AccountForm.js
@@ -7,71 +7,103 @@ const CREATE_ACTION = "Create";
 function AccountForm(props) {
   return (
     <form onSubmit={props.handleSubmit}>
-      <TextInput
-        id='accountNumber'
-        label='Account Number'
-        name='accountNumber'
-        value={props.account.accountNumber || ""}
-        onChange={props.handleChange}
-        error={props.errors.accountNumber}
-      />
+      <div className='row mb-2'>
+        <div className='col'>
+          <TextInput
+            id='accountNumber'
+            label='Account Number'
+            name='accountNumber'
+            value={props.account.accountNumber || ""}
+            onChange={props.handleChange}
+            error={props.errors.accountNumber}
+          />
+        </div>
+      </div>
 
-      <TextInput
-        id='accountName'
-        label='Account Name'
-        name='accountName'
-        value={props.account.accountName || ""}
-        onChange={props.handleChange}
-        error={props.errors.accountName}
-      />
+      <div className='row mb-2'>
+        <div className='col'>
+          <TextInput
+            id='accountName'
+            label='Account Name'
+            name='accountName'
+            value={props.account.accountName || ""}
+            onChange={props.handleChange}
+            error={props.errors.accountName}
+          />
+        </div>
+      </div>
 
       <hr />
-      <p>Account Holder</p>
+      <div className='row mb-2'>
+        <div className='col'>
+          <p>Account Holder</p>
+        </div>
+      </div>
 
-      <TextInput
-        id='firstName'
-        label='First Name'
-        name='firstName'
-        value={props.account.accountHolder.firstName || ""}
-        onChange={props.handleAccountHolderChange}
-        error={props.errors.firstName}
-      />
+      <div className='row mb-2'>
+        <div className='col'>
+          <TextInput
+            id='firstName'
+            label='First Name'
+            name='firstName'
+            value={props.account.accountHolder.firstName || ""}
+            onChange={props.handleAccountHolderChange}
+            error={props.errors.firstName}
+          />
+        </div>
+      </div>
 
-      <TextInput
-        id='middleName'
-        label='Middle Name'
-        name='middleName'
-        value={props.account.accountHolder.middleName || ""}
-        onChange={props.handleAccountHolderChange}
-        error={props.errors.middleName}
-      />
+      <div className='row mb-2'>
+        <div className='col'>
+          <TextInput
+            id='middleName'
+            label='Middle Name'
+            name='middleName'
+            value={props.account.accountHolder.middleName || ""}
+            onChange={props.handleAccountHolderChange}
+            error={props.errors.middleName}
+          />
+        </div>
+      </div>
 
-      <TextInput
-        id='lastName'
-        label='Last Name'
-        name='lastName'
-        value={props.account.accountHolder.lastName || ""}
-        onChange={props.handleAccountHolderChange}
-        error={props.errors.lastName}
-      />
+      <div className='row mb-2'>
+        <div className='col'>
+          <TextInput
+            id='lastName'
+            label='Last Name'
+            name='lastName'
+            value={props.account.accountHolder.lastName || ""}
+            onChange={props.handleAccountHolderChange}
+            error={props.errors.lastName}
+          />
+        </div>
+      </div>
 
       <hr />
       {props.action === CREATE_ACTION ? (
-        <NumberInput
-          id='initialDeposit'
-          label='Initial Deposit'
-          name='balanceAmount'
-          min={props.minDeposit}
-          max='99999'
-          value={props.account.balanceAmount || ""}
-          onChange={props.handleChange}
-          error={props.errors.balanceAmount}
-        />
+        <div className='row mb-2'>
+          <div className='col'>
+            <NumberInput
+              id='initialDeposit'
+              label='Initial Deposit'
+              name='balanceAmount'
+              min={props.minDeposit}
+              max='99999'
+              value={props.account.balanceAmount || ""}
+              onChange={props.handleChange}
+              error={props.errors.balanceAmount}
+            />
+          </div>
+        </div>
       ) : (
         ""
       )}
 
-      <input type='submit' value='Save' />
+      <div className='row text-center mt-3'>
+        <div className='col'>
+          <input type='submit' value='Save' className='btn btn-primary w-100' />
+        </div>
+      </div>
     </form>
   );
 }
