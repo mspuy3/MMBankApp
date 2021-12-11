21c65491418aa8dc3a315150d1a5a2023d18f8ca
Michael Siemens Uy <89561283+mspuy3@users.noreply.github.com>
2021-12-11 11:09:14 +0800

    revamped admin dashboard

--- bank-app/src/components/Admin/AdminDashboardPage.js
+++ bank-app/src/components/Admin/AdminDashboardPage.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Overview from "./Overview";
 import TransactionsListAll from "./TransactionsListAll";
+import TransactionsListAll2 from "./TransactionsListAll2";
 
 function AdminDashboardPage() {
   return (
@@ -7,22 +9,27 @@ function AdminDashboardPage() {
 
     <h1 className="text-center text-primary">Welcome to BPO!</h1>
     
-    <div className="container-fluid col-md-10 offset-md-1 shadow p-3 mb-5 bg-white rounded"
-    
-    >
-      <h2 className="text-center text-secondary">List of All Transactions</h2>
-
-      <div className="overflow-scroll overflow-hidden" style={{
-      height: "50vh"
-      }}
-      >
-        <TransactionsListAll />
-      </div>
-
-    </div>
+    {/* <Overview /> */}
+    <TransactionsListAll2 />
     
   </div>
   );
 }
 
 export default AdminDashboardPage;
+
+// Old implementation
+
+{/* <div className="container-fluid col-md-10 offset-md-1 shadow p-3 mb-5 bg-white rounded"
+    
+>
+  <h2 className="text-center text-secondary">List of All Transactions</h2>
+
+  <div className="overflow-scroll overflow-hidden" style={{
+  height: "50vh"
+  }}
+  >
+    <TransactionsListAll />
+  </div>
+
+</div> */}
